79c51573b9791614a51934eaeea297d9b878daa5
cgranja28 <cgranja@espol.edu.ec>
2021-01-10 17:54:26 -0500

    version 1.3.1 - Correcciones Version 1.3

--- src/main/java/com/mycompany/modelo/Alineacion.java
+++ src/main/java/com/mycompany/modelo/Alineacion.java
@@ -7,6 +7,7 @@ public class Alineacion {
     private String nombre;
     private ArrayList<List<Integer>> combinaciones;
 
+    //CONSTRUCTOR
     public Alineacion(int id) {
         this.id = id;
         Scanner sc= new Scanner(System.in);
@@ -15,7 +16,7 @@ public class Alineacion {
             try{ 
             int op = id;
             switch (op){
-              //****************************************ALNACION 1*****************************************************
+              //****************************************ALINEACION 1*****************************************************
               case 1:
                 nombre="FILA";
                 combinaciones= new ArrayList<List<Integer>>();
@@ -24,27 +25,24 @@ public class Alineacion {
                 combinaciones.add(new ArrayList<>(Arrays.asList(9, 10, 11, 12)));
                 combinaciones.add(new ArrayList<>(Arrays.asList(13, 14, 15, 16)));
                 break;
-              //****************************************ALNACION 2*****************************************************
+              //****************************************ALINEACION 2*****************************************************
               case 2:
                 nombre="COLUMNA";
-                combinaciones= new ArrayList<List<Integer>>();
                 combinaciones= new ArrayList<List<Integer>>();
                 combinaciones.add(new ArrayList<>(Arrays.asList(1, 5, 9, 13)));
                 combinaciones.add(new ArrayList<>(Arrays.asList(2, 6, 10, 14)));
                 combinaciones.add(new ArrayList<>(Arrays.asList(3, 7, 11, 15)));
                 combinaciones.add(new ArrayList<>(Arrays.asList(4, 8, 12, 16)));
                 break;
-              //****************************************ALNACION 3*****************************************************
+              //****************************************ALINEACION 3*****************************************************
               case 3:
                 nombre="ESQUINAS";
                 combinaciones= new ArrayList<List<Integer>>();
-                combinaciones= new ArrayList<List<Integer>>();
                 combinaciones.add(new ArrayList<>(Arrays.asList(1, 4, 13,16)));
                 break;
-              //****************************************ALNACION 4*****************************************************
+              //****************************************ALINEACION 4*****************************************************
               case 4:
                 nombre="JUNTAS 4 EN ESQUINAS";
-                combinaciones= new ArrayList<List<Integer>>();
                 combinaciones= new ArrayList<List<Integer>>();
                 combinaciones.add(new ArrayList<>(Arrays.asList(1, 2, 5, 6)));
                 combinaciones.add(new ArrayList<>(Arrays.asList(3, 4, 7, 8)));
@@ -52,11 +50,11 @@ public class Alineacion {
                 combinaciones.add(new ArrayList<>(Arrays.asList(11, 12, 15, 16)));
                 break; 
               default:
-                System.out.println("\nOPCION INVALIDA DE ALINACION\n");
+                System.out.println("\nOPCION INVALIDA DE ALINEACION\n");
                 break;
               }
             }catch (InputMismatchException e) {
-                System.out.println("\n**OPCION INVALIDA DE ALINACION**\n");
+                System.out.println("\n**OPCION INVALIDA DE ALINEACION**\n");
                 sc.next();
             }  
     }
